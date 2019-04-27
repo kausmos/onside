de114285edf77245482aac55b76bbfc3b8fad4e3
Kaustuva Ghosh <ghosh.kaustuva@gmail.com>
2019-04-27 10:56:19 +0000

    switch db

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ app.use(function(req,res,next){
 
 
 //------------------connecting to mongo server--------------------
-mongoose.connect("mongodb://localhost/onside",{useNewUrlParser: true});
-// mongoose.connect("mongodb://kaustuva:T4X1DR1V3R@ds147566.mlab.com:47566/onside",{useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost/onside",{useNewUrlParser: true});
+mongoose.connect("mongodb://kaustuva:T4X1DR1V3R@ds147566.mlab.com:47566/onside",{useNewUrlParser: true});
 
 io.on('connection', function(socket){
   
